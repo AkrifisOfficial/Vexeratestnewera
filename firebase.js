62df7209ca4b638234b5d16f17b454c8853952b0
Akrifstg <akrifisofficial@gmail.com>
2025-08-06 00:54:32 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0v92q_Fq_iED1Sbu2xug7hZjQUiK4vH8",
+    authDomain: "vexeradubbing-a8a40.firebaseapp.com",
+    projectId: "vexeradubbing-a8a40",
+    storageBucket: "vexeradubbing-a8a40.firebasestorage.app",
+    messagingSenderId: "977285669703",
+    appId: "1:977285669703:web:0e479be8f28eb2b093a262"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
